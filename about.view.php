609653e5aa41f5a9dc2155880cc7a05ebe8fa721
ahed1854 <12345makemoney12345@gmail.com>
2024-11-23 11:30:00 -0500

    added new page design, and made it compatibale with MVC model, will make it dynamic with PHP later.

--- about.view.php
+++ about.view.php
@@ -120,12 +120,12 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">About</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <p>Hello. Welcome to the home page.</p>
+                <p>About page.</p>
             </div>
         </main>
     </div>
--- index.php
+++ index.php
@@ -1,31 +1,3 @@
 <?php
 
-$books = [
-    [
-
-        "name" => "Do Androids dream of Electric Sheeps",
-        "author" => "Philip K. Dick",
-        "releaseYear" => 1968,
-        "purchaseUrl" => "https://example.com"
-    ],
-    [
-
-        "name" => "The Langoliers",
-        "author" => "Andy Weir",
-        "releaseYear" => 2021,
-        "purchaseUrl" => "https://example.com"
-    ],
-    [
-
-        "name" => "The Martian",
-        "author" => "Andy Weir",
-        "releaseYear" => 2011,
-        "purchaseUrl" => "https://example.com"
-    ],
-];
-
-$filteredBooks = array_filter($books, function ($books) {
-    return $books["author"] === "Andy Weir";
-});
-
 require "index.view.php";
