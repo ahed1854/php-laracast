4e97f8ab3fd0574c2866e1663d3494e7777cb877
ahed1854 <12345makemoney12345@gmail.com>
2024-11-24 11:57:33 -0500

    modified the code to make it more suitable with the functions we made, so it'd be more dynamic and readable

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,9 +8,9 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/websites/demo/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                        <a href="/websites/demo/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About Us</a>
-                        <a href="/websites/demo/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                        <a href="/websites/demo/" class="<?= urlIs("/websites/demo/") ? "bg-gray-900 text-white" : "text-gray-300"; ?> hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+                        <a href="/websites/demo/about.php" class="<?= urlIs("/websites/demo/about.php") ? "bg-gray-900 text-white" : "text-gray-300"; ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About Us</a>
+                        <a href="/websites/demo/contact.php" class="<?= urlIs("/websites/demo/contact.php") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
                     </div>
                 </div>
             </div>
