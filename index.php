ee6fd6819a7a1f90f9e47065163a71358b18aea1
ahed1854 <12345makemoney12345@gmail.com>
2024-11-25 10:30:40 -0500

    introducing the concepts of Classes, and connecting the database we made earlier, and learnt how to type in queries and fetch data

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
+
+// connect to MySql database
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+// this will prepare the query we're going to send
+$statement = $pdo->prepare("select * from posts");
+
+// executing the query
+$statement->execute();
+
+// fetching the resaults
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post["title"] . "</li>";
+}
