d5ffc29ec25c80d30ec5830d3ea1d3ca92a42947
ahed1854 <12345makemoney12345@gmail.com>
2024-11-25 11:29:55 -0500

    created new instance of the Database class and executed a query

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
 <?php
 
 require "functions.php";
+require "Database.php";
 // require "router.php";
 
-// connect to MySql database
+$db = new Database;
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-
-$pdo = new PDO($dsn);
-
-// this will prepare the query we're going to send
-$statement = $pdo->prepare("select * from posts");
-
-// executing the query
-$statement->execute();
-
-// fetching the resaults
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post["title"] . "</li>";
-}
+dd($posts);
